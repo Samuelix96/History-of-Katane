4829b7e55d8fb50457fe3239b40af0119ed6cbeb
Samuelix96 <131618047+Samuelix96@users.noreply.github.com>
2024-02-26 00:01:28 +0100

    new title

--- fe/src/components/article/Article.jsx
+++ fe/src/components/article/Article.jsx
@@ -1,32 +1,29 @@
+/** @format */
 
-import React from 'react'
-import "./article.css"
+import React from 'react';
+import './article.css';
 
-const Article = ({ img, description, title, source, subtitle, reverse }) =>
-{
-  
-    const articleClass = `parallax-article ${ reverse ? 'reverse' : '' }`;
+const Article = ({ img, description, title, source, subtitle, reverse }) => {
+  const articleClass = `parallax-article ${reverse ? 'reverse' : ''}`;
 
-    return (
-        <div className='container-fluid my-4 text-start'>
-            
-            <div className={ articleClass }>
-
-                <div className="article-image">
-                    
-                    <img src={ img } alt="Immagine dell'articolo" />
-                </div>
-                <div className="article-description">
-                <h1 className='h1_article'>{title}</h1>
-                    <h3 className='h3_article'>{ subtitle }</h3>
-                    <p>{ description }</p>
-                    <span>{ source }</span>
-                </div>
-            </div>
+  return (
+    <div className='container-fluid my-4 text-start'>
+      <div className={articleClass}>
+        <div className='article-image'>
+          <img
+            src={img}
+            alt="Immagine dell'articolo"
+          />
         </div>
-
-    )
-}
+        <div className='article-description'>
+          <h1 className='h1_article'>{title}</h1>
+          <h3 className='h3_article'>{subtitle}</h3>
+          <p>{description}</p>
+          <span>{source}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Article;
-
